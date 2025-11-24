c40f0824ee73f0639dcb8e69c14b97a699306bdc
SowmyaRavi29 <sowmyaravi0429@gmail.com>
2025-11-24 16:07:40 +0530

    Create userRegistration20.java

---
+++ src/myApplication/userRegistration20.java
@@ -0,0 +1,32 @@
+
+package moodAnalyzer;
+
+import java.util.regex.Pattern;
+
+public class userRegistration20 {
+    // Regex Patterns
+    private static final String NAME_PATTERN = "^[A-Z][a-zA-Z]{2,}$";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([._%+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\\.[a-zA-Z]{2,})+$";
+    private static final String MOBILE_PATTERN = "^[0-9]{2}\\s[0-9]{10}$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}$";
+
+    public boolean validateFirstName(String firstName) {
+        return Pattern.matches(NAME_PATTERN, firstName);
+    }
+
+    public boolean validateLastName(String lastName) {
+        return Pattern.matches(NAME_PATTERN, lastName);
+    }
+
+    public boolean validateEmail(String email) {
+        return Pattern.matches(EMAIL_PATTERN, email);
+    }
+
+    public boolean validateMobile(String mobile) {
+        return Pattern.matches(MOBILE_PATTERN, mobile);
+    }
+
+    public boolean validatePassword(String password) {
+        return Pattern.matches(PASSWORD_PATTERN, password);
+    }
+}
